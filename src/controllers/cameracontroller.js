92a6ad38a035d643e3114c3c0a74c95e33c85729
vik <victoret86@gmail.com>
2015-01-19 17:42:08 +0100

    working on rotations with quats

--- src/controllers/cameracontroller.js
+++ src/controllers/cameracontroller.js
@@ -22,10 +22,24 @@ EZ.CameraController = function ( renderer ) {
     this.scale = 1.0;
     this.zoom_speed = 1.0;
 
-    // the scope changed if we enter through an event, thus we need the var that
+    // the scope changes if we enter through an event, thus we need the var that
     var that = this;
     this.onMouseMove = function (e) {
+        if(e.dragging){
+            var delta = e.deltax > 0.1 || e.deltax < -0.1 ? -e.deltax : 0;
+            if(delta)
+                quat.setAxisAngle( that.cam.quat, [0,1,0], delta * DEG2RAD );
+            else
+                quat.identity(that.cam.quat);
+            delta = e.deltay > 0.1 || e.deltay < -0.1 ? -e.deltay : 0;
+            if(delta)
+                quat.setAxisAngle( EZ.temp_quat, [1,0,0], delta * DEG2RAD );
+            else
+                quat.identity(EZ.temp_quat);
 
+            quat.mul(that.cam.quat,EZ.temp_quat, that.cam.quat);
+            that.needs_update = true;
+        }
     };
 
     this.onMouseDown = function (e) {
@@ -42,10 +56,16 @@ EZ.CameraController = function ( renderer ) {
     this.update = function (dt) {
         this.cam = this.renderer.current_cam;
         if(this.cam && this.needs_update){
+            // computations for the zoom, EZ.temp_vec4 is the new radius
             vec3.sub(EZ.temp_vec4,this.cam.position, this.target);
             vec3.scale(EZ.temp_vec4, EZ.temp_vec4, this.scale);
+
+
+            vec3.transformQuat(EZ.temp_vec4, EZ.temp_vec4,that.cam.quat );
+
             vec3.add(this.cam.position,this.target, EZ.temp_vec4 );
 
+            quat.identity(that.cam.quat);
             this.scale = 1.0;
             this.cam.lookAt(this.target);
             this.needs_update = false;
